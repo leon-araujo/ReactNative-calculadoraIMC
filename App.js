b4f03dc21fb074e120108085df3f028187f5b651
leon-araujo <leonardo.8065@gmail.com>
2021-12-03 22:18:48 -0300

    modificando o app.json

--- App.js
+++ App.js
@@ -1,78 +1,88 @@
 import React, {useState} from "react";
-import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import { View ,  Text, StyleSheet, TextInput , TouchableOpacity} from 'react-native';
 
-export default function App() {
-  const [peso, setPeso] = useState(''); // armazena o peso
-  const [altura, setAltura] = useState(''); // armazena o peso
+export default function app() {
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
 
   function executarCalculos() {
     const alt = altura / 100;
     const imc = peso / (alt * alt);
-    alert ('Seu indice de massa corporal é: ' + imc.toFixed(2));
+    if (imc < 18.6) {
+      alert('Você esta abaixo do peso - IMC = ' + imc.toFixed(2));
+    } else if (imc >= 18.6 && imc < 24.9) {
+      alert('Você esta com o peso ideal - IMC = ' + imc.toFixed(2));
+    } else if (imc >= 24.9 && imc < 34.9) {
+      alert('Você está levemente acima do peso - IMC = ' + imc.toFixed(2));
+    } else if (imc > 34.9) {
+      alert('Você está acima do peso - IMC = ' + imc.toFixed(2));
+    }
+
+    setPeso('');    // limpa os campos apos o resutado
+    setAltura('');  // limpa os campos apos o resutado
   }
 
-  return(
+  return (
     <View style={estilo.container}>
       <Text style={estilo.title}>Calcule seu IMC</Text>
-
       <TextInput
-        style={estilo.input} 
-        value={peso}              // valor dentro do componente
-        onChangeText={(peso) => setPeso(peso)}       // toda a vez que o campo mudar ele é salvo
+        style={estilo.input}
+        value={peso}                             // valor dentro do componente
+        onChangeText={ (peso) => setPeso(peso)}  // toda vez que o campo muda ele é salvo
         placeholder="Peso (kg)"
-        keyboardType='numeric'
+        keyboardType="numeric"
       />
 
       <TextInput
-        style={estilo.input} 
-        value={altura}              // valor dentro do componente
-        onChangeText={(altura) => setAltura(altura)}       // toda a vez que o campo mudar ele é salvo
+        style={estilo.input}
+        value={altura}                                // valor dentro do componente
+        onChangeText={ (altura) => setAltura(altura)} // toda vez que o campo muda ele é salvo
         placeholder="Altura (cm)"
-        keyboardType='numeric'
+        keyboardType="numeric"
       />
-
-      <TouchableOpacity onPress={executarCalculos} style={estilo.botao}>
+      <TouchableOpacity style={estilo.botao} onPress={executarCalculos}>
         <Text style={estilo.textBotao}>Calcular</Text>
       </TouchableOpacity>
-
     </View>
   );
 }
 
-const estilo = StyleSheet.create ({
+{/*  ESTILIZAÇÂO DO APP
+      ||           ||
+      ||           ||
+      ||           ||
+      \/           \/   */}
+
+const estilo = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#ddd',
+    flex: 1
   },
 
   title: {
     textAlign: 'center',
-    marginTop: 25,
-    fontSize: 30
+    marginTop: 30,
+    fontSize: 25
   },
 
   input: {
-    backgroundColor: '#B10000',
+    backgroundColor: '#ddd',
     borderRadius: 10,
     margin: 15,
     padding: 10,
-    color: '#FFE5DA',
+    color: '#000',
     fontSize: 23
   },
 
   botao: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'center' ,
+    alignItems: 'center', 
     margin: 15,
-    backgroundColor: '#007C51',
+    backgroundColor: '#41AEF4',
     padding: 10,
-    borderRadius: 10,
   },
 
   textBotao: {
-    color: "#fff",
-    fontSize: 25,
-    fontWeight: 'bold',
-    fontFamily: 'Arial'
+    color: '#fff',
+    fontSize: 25
   }
 });
